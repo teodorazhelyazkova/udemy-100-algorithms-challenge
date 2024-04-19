a4e9bd26788dc8c1a625f37aa742ff031073a8f8
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-04-19 16:45:53 +0300

    addBorder solution

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,3 +1,9 @@
-export function addBorder(picture: string[]): string[] {}
+export function addBorder(picture: string[]): string[] {
+  const wall = "*".repeat(picture[0].length + 2);
+
+  const borderedPicture = [wall, ...picture.map((line) => `*${line}*`), wall];
+
+  return borderedPicture;
+}
 
 // console.log(addBorder(["abc", "ded"]));
