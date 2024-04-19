b94a1dca8d97ba731acf4d1e98a1c90f99feabbd
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-04-19 16:45:00 +0300

    add solution

--- add/add.ts
+++ add/add.ts
@@ -1,6 +1,10 @@
-export function add(param1: number, param2: number): number {}
+export function add(param1: number, param2: number): number {
+  return param1 + param2;
+}
 
-export function add2(...param1: number[]): number {}
+export function add2(...param1: number[]): number {
+  return param1.reduce((acc, num) => acc + num, 0);
+}
 // console.log(add(1, 2));
 // console.log(add(3, 2));
 
