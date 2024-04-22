8161b481c090c53a1f24d6615a2c15dbf81a6be5
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-04-22 14:50:00 +0300

    allLongestStrings solution

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,3 +1,18 @@
-export function allLongestStrings(inputArray: string[]): string[] {}
+export function allLongestStrings(inputArray: string[]): string[] {
+  let maxLength = 0;
+  const longestStrings: string[] = [];
+
+  inputArray.forEach((string) => {
+    if (string.length > maxLength) {
+      maxLength = string.length;
+      longestStrings.splice(0);
+      longestStrings.push(string);
+    } else if (string.length === maxLength) {
+      longestStrings.push(string);
+    }
+  });
+
+  return longestStrings;
+}
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
