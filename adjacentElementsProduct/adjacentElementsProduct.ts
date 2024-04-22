c4bbc98128355aa17b998041b44ed1378b9b3bc8
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-04-22 10:28:40 +0300

    adjacentElementsProduct solution

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,3 +1,13 @@
-export function adjacentElementsProduct(inputArray: number[]): number {}
+export function adjacentElementsProduct(inputArray: number[]): number {
+  let largestProduct = inputArray[0] * inputArray[1];
+
+  for (let i = 1; i < inputArray.length - 1; i++) {
+    const currentProduct = inputArray[i] * inputArray[i + 1];
+
+    largestProduct = Math.max(largestProduct, currentProduct);
+  }
+
+  return largestProduct;
+}
 
 //console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
