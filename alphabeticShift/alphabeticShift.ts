0253c3e3f1c66a24eb1f78184a4a03ac67af69eb
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-04-23 12:37:04 +0300

    alphabeticShift solution

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,3 +1,11 @@
-export function alphabeticShift(inputString: string): string {}
+export function alphabeticShift(inputString: string): string {
+  return inputString.replace(/[a-z]/g, (character) => {
+    if (character === "z") {
+      return "a";
+    }
+
+    return String.fromCharCode(character.charCodeAt(0) + 1);
+  });
+}
 
 console.log(alphabeticShift("crazy"));
