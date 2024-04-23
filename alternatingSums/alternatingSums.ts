7d09efa52ac1d34fc7f57b191fa1a2d3ec19a224
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-04-23 15:39:31 +0300

    alternatingSum solution

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,3 +1,19 @@
-export function alternatingSums(a: number[]): number[] {}
+export function alternatingSums(a: number[]): number[] {
+  let team1: number[] = [];
+  let team2: number[] = [];
+
+  a.forEach((weight, index) => {
+    if (index % 2 === 0) {
+      team1.push(weight);
+    } else {
+      team2.push(weight);
+    }
+  });
+
+  return [
+    team1.reduce((acc, currentValue) => acc + currentValue),
+    team2.reduce((acc, currentValue) => acc + currentValue),
+  ];
+}
 
 console.log(alternatingSums([50, 60, 60, 45, 70]));
