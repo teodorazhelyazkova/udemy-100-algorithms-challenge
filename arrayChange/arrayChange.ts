514a54ddd0e6bc45bcfef498f646ad9d34d72d5a
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-04-24 11:15:45 +0300

    arrayChange solution

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,3 +1,15 @@
-export function arrayChange(inputArray: number[]): number {}
+export function arrayChange(inputArray: number[]): number {
+  let moves = 0;
+
+  for (let i = 1; i < inputArray.length; i++) {
+    if (inputArray[i] <= inputArray[i - 1]) {
+      const diff = inputArray[i - 1] - inputArray[i] + 1;
+      moves += diff;
+      inputArray[i] += diff;
+    }
+  }
+
+  return moves;
+}
 
 // console.log(arrayChange([1, 1, 1]));
