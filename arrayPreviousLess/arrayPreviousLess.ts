3a8e64f26075914e8a9ee3c797afe7542271f4ff
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-05-07 09:56:20 +0300

    arrayPreviousLess solution

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,3 +1,23 @@
-export function arrayPreviousLess(items: number[]): number[] {}
+export function arrayPreviousLess(items: number[]): number[] {
+  const result: number[] = [];
+
+  for (let i = 0; i < items.length; i++) {
+    let found = false;
+
+    for (let j = i - 1; j >= 0; j--) {
+      if (items[i] > items[j]) {
+        result.push(items[j]);
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      result.push(-1);
+    }
+  }
+
+  return result;
+}
 
 // console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
