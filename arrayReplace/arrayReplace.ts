1db6006b5c78289aea9a65972dbc66be8e03e500
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-05-07 10:05:23 +0300

    arrayReplace solution

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,7 +1,11 @@
 export function arrayReplace(
   inputArray: number[],
   elemToReplace: number,
-  substitutionElem: number,
-): number[] {}
+  substitutionElem: number
+): number[] {
+  return inputArray.map((element) =>
+    element === elemToReplace ? substitutionElem : element
+  );
+}
 
 // console.log(arrayReplace([1, 2, 1], 1, 3));
