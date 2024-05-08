783cb66ff728919fdbb77a3e8e4b94bdaf298757
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-05-08 23:23:17 +0300

    avoidObstacles solution

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,3 +1,13 @@
-export function avoidObstacles(inputArray: number[]): number {}
+export function avoidObstacles(inputArray: number[]): number {
+  const maxJump = Math.max(...inputArray) + 1;
+
+  for (let jump = 1; jump <= maxJump; jump++) {
+    if (inputArray.every((obstacle) => obstacle % jump !== 0)) {
+      return jump;
+    }
+  }
+
+  return maxJump;
+}
 
 // console.log(avoidObstacles([5, 3, 6, 7, 9]));
