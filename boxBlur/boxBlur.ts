4a454f7a2d769b25135466a7b3890f8c000b2df4
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-05-13 22:06:06 +0300

    boxBlur solution

--- boxBlur/boxBlur.ts
+++ boxBlur/boxBlur.ts
@@ -1,5 +1,30 @@
-export function boxBlur(image: number[][]): number[][] {}
+export function boxBlur(image: number[][]): number[][] {
+  const numRows = image.length - 2;
+  const numCols = image[0].length - 2;
+  const blurredImage: number[][] = [];
 
-// console.log(boxBlur([[1, 1, 1],
-//     [1, 7, 1],
-//     [1, 1, 1]]));
+  for (let i = 0; i < numRows; i++) {
+    blurredImage[i] = [];
+
+    for (let j = 0; j < numCols; j++) {
+      let sum = 0;
+
+      for (let row = i; row < i + 3; row++) {
+        for (let col = j; col < j + 3; col++) {
+          sum += image[row][col];
+        }
+      }
+      blurredImage[i][j] = Math.floor(sum / 9);
+    }
+  }
+
+  return blurredImage;
+}
+
+console.log(
+  boxBlur([
+    [1, 1, 1],
+    [1, 7, 1],
+    [1, 1, 1],
+  ])
+);
