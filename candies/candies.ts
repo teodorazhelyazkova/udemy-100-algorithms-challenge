2e5a9377f0ccc3b5a865b40d482032252a16e567
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-05-14 16:48:45 +0300

    candies solution

--- candies/candies.ts
+++ candies/candies.ts
@@ -1,3 +1,14 @@
-export function candies(n: number, m: number): number {}
+export function candies(n: number, m: number): number {
+    const candies = m;
+    const children = n;
+
+    if (children > candies) {
+        return 0;
+    } else {
+        const candiesPerChild = Math.trunc(candies / children);
+
+        return candiesPerChild * children;
+    }
+}
 
 // console.log(candies(3, 10));
