4f56ff1f83483d1cf0fa6f369a9718f8e4144934
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-05-16 17:21:18 +0300

    characterParity solution

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,5 +1,11 @@
-export function characterParity(symbol: string): string {}
+export function characterParity(symbol: string): string {
+    const isDigit = !isNaN(Number(symbol));
 
-// console.log(characterParity('5'))
-// console.log(characterParity('8'))
-// console.log(characterParity('q'))
+    if (!isDigit) return 'not a digit';
+
+    return Number(symbol) % 2 === 0 ? 'even' : 'odd';
+}
+
+console.log(characterParity('5'));
+console.log(characterParity('8'));
+console.log(characterParity('q'));
