cb7fe0f3829d055ee9d4da53ff603cb57052d474
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-05-17 20:45:35 +0300

    second solution for checking palindromes

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,4 +1,20 @@
-export function checkPalindrome(inputString: string): boolean {}
+export function checkPalindrome(inputString: string): boolean {
+    // Define regex to remove non-alphanumeric characters
+    const regex = /[^A-Za-z0-9]/g;
+
+    // Normalize the input string
+    const normalizedString = inputString.toLowerCase().replace(regex, '');
+    const length = normalizedString.length;
+
+    // Check if the string is a palindrome
+    for (let i = 0; i < length / 2; i++) {
+        if (normalizedString[i] !== normalizedString[length - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 // console.log(checkPalindrome('aabaa'));
 // console.log(checkPalindrome('abac'));
