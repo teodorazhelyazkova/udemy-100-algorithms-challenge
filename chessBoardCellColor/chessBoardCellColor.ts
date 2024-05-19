3afe7688d46de1884e6c64cdfb6b8f07f5709f7d
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-05-19 21:38:48 +0300

    chessBoardCellColor solution

--- chessBoardCellColor/chessBoardCellColor.ts
+++ chessBoardCellColor/chessBoardCellColor.ts
@@ -1,4 +1,14 @@
-export function chessBoardCellColor(cell1: string, cell2: string): boolean {}
+export function chessBoardCellColor(cell1: string, cell2: string): boolean {
+    const getCellColor = (cell: string) => {
+        const [col, row] = cell.split('');
+        const colValue = col.charCodeAt(0) - 'A'.charCodeAt(0) + 1;
+        const rowValue = parseInt(row);
+
+        return (colValue + rowValue) % 2;
+    };
+
+    return getCellColor(cell1) === getCellColor(cell2);
+}
 
 // console.log(chessBoardCellColor('A1', 'C3'));
 // console.log(chessBoardCellColor('A1', 'H3'));
