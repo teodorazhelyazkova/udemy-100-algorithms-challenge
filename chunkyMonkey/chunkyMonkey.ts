ff5373ce25c84df543aceb949626533be53af46f
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-05-20 15:24:01 +0300

    chunkyMonkey solution

--- chunkyMonkey/chunkyMonkey.ts
+++ chunkyMonkey/chunkyMonkey.ts
@@ -1,4 +1,12 @@
-export function chunkyMonkey(arr: any[], size: number): any[][] {}
+export function chunkyMonkey(arr: any[], size: number): any[][] {
+    const chunks = [];
+
+    for (let i = 0; i < arr.length; i += size) {
+        chunks.push(arr.slice(i, i + size));
+    }
+
+    return chunks;
+}
 
 // console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
 // console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
