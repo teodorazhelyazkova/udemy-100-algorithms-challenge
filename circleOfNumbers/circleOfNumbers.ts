4358cc7b43260d921c1c46d6870af80c37029972
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-05-25 21:08:35 +0300

    circleOfNumbers solution

--- circleOfNumbers/circleOfNumbers.ts
+++ circleOfNumbers/circleOfNumbers.ts
@@ -1,3 +1,8 @@
-export function circleOfNumbers(n: number, firstNumber: number): number {}
+export function circleOfNumbers(n: number, firstNumber: number): number {
+    // Add half of n to firstNumber in order to push
+    // the value halfway around the circle, then mod by n
+    // to restrict the result to the boundary between 0 and n-1.
+    return (firstNumber + n / 2) % n;
+}
 
 // console.log(circleOfNumbers(10, 2));
