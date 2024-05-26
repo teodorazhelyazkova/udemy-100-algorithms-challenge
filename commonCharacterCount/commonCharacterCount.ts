42414626af6b84bfa08547c5c3e414a0e0ec1a80
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-05-26 20:39:24 +0300

    commonCharacterCount solution

--- commonCharacterCount/commonCharacterCount.ts
+++ commonCharacterCount/commonCharacterCount.ts
@@ -1,3 +1,16 @@
-export function commonCharacterCount(s1: string, s2: string): number {}
+export function commonCharacterCount(s1: string, s2: string): number {
+    let count = 0;
+    const arrayFromS1 = Array.from(s1);
+    const arrayFromS2 = Array.from(s2);
+
+    arrayFromS1.forEach((e) => {
+        if (arrayFromS2.includes(e)) {
+            count++;
+            arrayFromS2.splice(arrayFromS2.indexOf(e), 1);
+        }
+    });
+
+    return count;
+}
 
 // console.log(commonCharacterCount('aabcc', 'adcaa'));
