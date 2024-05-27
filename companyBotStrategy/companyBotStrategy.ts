defdae505091be76ca3dd4bb98f1786c2c0abf3f
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-05-27 15:53:24 +0300

    companyBotStrategy solution

--- companyBotStrategy/companyBotStrategy.ts
+++ companyBotStrategy/companyBotStrategy.ts
@@ -1,4 +1,15 @@
-export function companyBotStrategy(trainingData: number[][]): number {}
+export function companyBotStrategy(trainingData: number[][]): number {
+    const correctValues = trainingData
+        .filter(([answerTime, correctness]) => correctness > 0)
+        .map(([time]) => time);
+
+    const totalCorrectValues = correctValues.reduce(
+        (acc, time) => acc + time,
+        0
+    );
+
+    return correctValues.length ? totalCorrectValues / correctValues.length : 0;
+}
 
 // console.log(companyBotStrategy([[3, 1], [6, 1], [4, 1], [5, 1]]));
 // console.log(companyBotStrategy([[4, 1], [4, -1], [0, 0], [6, 1]]));
