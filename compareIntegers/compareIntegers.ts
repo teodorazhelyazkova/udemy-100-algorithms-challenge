1bc6c2e8dbb62061a4b108946e114365f7fd590c
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-05-28 20:04:45 +0300

    compareIntegers solution

--- compareIntegers/compareIntegers.ts
+++ compareIntegers/compareIntegers.ts
@@ -1,4 +1,15 @@
-export function compareIntegers(a: string, b: string): string {}
+export function compareIntegers(a: string, b: string): string {
+    const intA = parseInt(a);
+    const intB = parseInt(b);
+
+    if (intA > intB) {
+        return 'greater';
+    } else if (intA < intB) {
+        return 'less';
+    } else {
+        return 'equal';
+    }
+}
 
 // console.log(compareIntegers('12', '13'));
 // console.log(compareIntegers('875', '799'));
