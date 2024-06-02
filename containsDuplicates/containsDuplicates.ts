98d96006a08547f730e42661c9ea2489d60f9e79
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-06-02 22:42:51 +0300

    containsDuplicates solution

--- containsDuplicates/containsDuplicates.ts
+++ containsDuplicates/containsDuplicates.ts
@@ -1,4 +1,19 @@
-export function containsDuplicates(a: number[]): boolean {}
+export function containsDuplicates(a: number[]): boolean {
+    if (a.length <= 1) {
+        return false;
+    }
+
+    const set: Set<number> = new Set<number>();
+
+    for (const num of a) {
+        if (set.has(num)) {
+            return true;
+        }
+        set.add(num);
+    }
+
+    return false;
+}
 
 // console.log(containsDuplicates([1, 2, 3, 1]));
 // console.log(containsDuplicates([3, 1]));
