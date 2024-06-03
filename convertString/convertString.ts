a0b155dff4de586cf53bdaadeadfec656abc9c61
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-06-03 17:52:18 +0300

    covertString solution

--- convertString/convertString.ts
+++ convertString/convertString.ts
@@ -1,4 +1,18 @@
-export function convertString(s: string, t: string): boolean {}
+export function convertString(s: string, t: string): boolean {
+    let tIndex = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === t[tIndex]) {
+            tIndex++;
+
+            if (tIndex === t.length) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
 
 // console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
 // console.log(convertString('addbyca', 'abcd'));
