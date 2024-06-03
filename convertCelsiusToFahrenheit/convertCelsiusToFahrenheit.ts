d1ab1c46b084eb5c98e3b4d1f25cff9667d12c91
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-06-03 17:33:46 +0300

    celsiusToFahrenheit solution

--- convertCelsiusToFahrenheit/convertCelsiusToFahrenheit.ts
+++ convertCelsiusToFahrenheit/convertCelsiusToFahrenheit.ts
@@ -1,4 +1,9 @@
-export function celsiusToFahrenheit(celsius: number): number {}
+export function celsiusToFahrenheit(celsius: number): number {
+    const factor = 9 / 5;
+    const offset = 32;
+
+    return celsius * factor + offset;
+}
 
 // console.log(celsiusToFahrenheit(-30));
 // console.log(celsiusToFahrenheit(-10));
