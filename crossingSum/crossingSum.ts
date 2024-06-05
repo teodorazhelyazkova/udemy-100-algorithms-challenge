8537e1c8b76c7370589fa28657121f69165d697d
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-06-05 15:37:19 +0300

    crossingSum solution

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,4 +1,20 @@
-export function crossingSum(matrix: number[][], a: number, b: number): number {}
+export function crossingSum(matrix: number[][], a: number, b: number): number {
+    // Sum of all elements in the a-th row
+    const rowSum = matrix[a].reduce((sum, value) => sum + value, 0);
+
+    // Sum of all elements in the b-th column
+    const colSum = matrix.reduce((sum, row) => sum + row[b], 0);
+
+    console.log('colSum', colSum);
+
+    // Subtract the intersecting element (matrix[a][b]) as it is added twice
+    const intersectingElement = matrix[a][b];
+
+    // Calculate the final sum
+    const result = rowSum + colSum - intersectingElement;
+
+    return result;
+}
 
 // console.log(crossingSum([[1, 1, 1, 1],
 //     [2, 2, 2, 2],
