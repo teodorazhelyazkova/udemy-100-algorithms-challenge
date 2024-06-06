7a9a8a218d4691524fbe92ccb3f38d049349e254
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-06-06 12:48:36 +0300

    depositProfit solution

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,7 +1,18 @@
 export function depositProfit(
-  deposit: number,
-  rate: number,
-  threshold: number,
-): number {}
+    deposit: number,
+    rate: number,
+    threshold: number
+): number {
+    const annualGrowthRateDecimal = rate / 100;
+    let years = 0;
+    let totalBalance = deposit;
+
+    while (totalBalance < threshold) {
+        totalBalance += totalBalance * annualGrowthRateDecimal;
+        years++;
+    }
+
+    return years;
+}
 
 // console.log(depositProfit(100, 20, 170))
