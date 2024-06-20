9be82dbd9ea428c31943dca17fb7a353bdd5cdcc
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-06-20 11:43:10 +0300

    digitDegree solution

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,4 +1,24 @@
-export function digitDegree(n: number): number {}
+const sumOfDigits = (num: number): number => {
+    return num
+        .toString()
+        .split('')
+        .reduce((sum, digit) => sum + parseInt(digit), 0);
+};
+
+export function digitDegree(n: number): number {
+    if (n < 10) {
+        return 0;
+    }
+
+    let degreeCount = 0;
+
+    while (n >= 10) {
+        n = sumOfDigits(n);
+        degreeCount++;
+    }
+
+    return degreeCount;
+}
 
 // console.log(digitDegree(5));
 // console.log(digitDegree(10));
