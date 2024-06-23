08e8e9899f28315d2761d5915da738ef8f88ded5
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-06-23 15:40:05 +0300

    domainTypes solution

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,3 +1,16 @@
-export function domainType(domains: string[]): string[] {}
+export function domainType(domains: string[]): string[] {
+    const domainLabels: { [key: string]: string } = {
+        org: 'organization',
+        com: 'commercial',
+        net: 'network',
+        info: 'information',
+    };
+
+    return domains.map((domain) => {
+        const extension = domain.split('.').pop() || '';
+
+        return domainLabels[extension] || 'unknown';
+    });
+}
 
 // console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
