f9392baf9dea1ff89efae0ffc1a6b22f2f1494ec
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-07-03 17:54:45 +0300

    electionWinners solution

--- electionWinners/electionWinners.ts
+++ electionWinners/electionWinners.ts
@@ -1,3 +1,22 @@
-export function electionsWinners(votes: number[], k: number): number {}
+export function electionsWinners(votes: number[], k: number): number {
+    const maxVote = Math.max(...votes);
+    const numOfCandidatesWithMaxVotes = votes.filter(
+        (vote) => vote === maxVote
+    ).length;
+
+    if (numOfCandidatesWithMaxVotes > 1) {
+        return 0;
+    }
+
+    let potentialWinners = 0;
+
+    for (const vote of votes) {
+        if (vote + k > maxVote) {
+            potentialWinners++;
+        }
+    }
+
+    return potentialWinners;
+}
 
 // console.log(electionsWinners([2, 3, 5, 2], 3));
